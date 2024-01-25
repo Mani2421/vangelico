164474e0793cc5479bd3a75931faae106b5c0625
Maney <70456176+Mani2421@users.noreply.github.com>
2024-01-25 17:30:52 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@
 Config = {}
 
 Config.Debug = false -- Additional info for testing and debugging
-Config.CooldownTime = 1800000 -- 30 Minute cooldown after robbery by default, change the value here
+Config.CooldownTimeInMinutes = 30 -- 30 Minute cooldown after robbery by default, change the value here
 Config.IsStoreOnCooldown = false -- Cooldown starts after the whole store has been robbed, we check this with areAllCasesRobbed()
 Config.WhitelistedWeapons = { -- Stores weapon hashes whitelisted for robbing case,
     GetHashKey("weapon_carbinerifle"),
