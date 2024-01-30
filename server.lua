82bd0baf37fc03a9c0cad37a65636bdabe70e9b5
Maney <70456176+Mani2421@users.noreply.github.com>
2024-01-30 11:21:38 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,3 +3,17 @@
     SERVER-SIDE 
     AUTHOR: MANI
 ]]
+
+---------------------------------------------------------
+-----------------------EVENTS----------------------------
+---------------------------------------------------------
+
+AddEventHandler("dispatch-police")
+RegisterServerEvent("dispatch-police", function()
+    local playerPed = PlayerPedId()
+    for _, case in pairs(Config.Cases) do
+        if case.isRobbed then -- If any of the cases get robbed...
+            -- Dispatch police
+        end
+    end
+end)
